71c157c57c94e0c17ff7ee48c9e49ce74e971b9c
TabrezGulobov <tabrezgulobov@gmail.com>
2024-12-06 19:05:07 +0500

    Add files via upload

---
+++ ConsoleApp36/ConsoleApp36/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp36
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            for (double i = 1; i <= n; i++)
+            {
+                sum += 1/i;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
+
